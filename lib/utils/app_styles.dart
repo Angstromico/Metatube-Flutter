74fbd83700aaa307970e5f11a8467d11e6c62ebb
Manuel Esteban Morales Zuarez <manuesteban1990@gmail.com>
2026-05-18 10:35:15 -0400

    style: medium color in app theme

--- lib/utils/app_styles.dart
+++ lib/utils/app_styles.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 class AppTheme {
   static const Color dark = Color(0xFF1E1E1E);
   static const Color light = Color(0xFFFFFFFF);
+  static const Color medium = Color(0xFF888888);
 
   static ThemeData get lightTheme {
     return ThemeData(
