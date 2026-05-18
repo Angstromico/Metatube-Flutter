bb65d3a1098cee4cda5a7cc27060d143dff0b321
Manuel Esteban Morales Zuarez <manuesteban1990@gmail.com>
2026-05-18 10:37:52 -0400

    style: use medium color in action button

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:metatube/utils/app_styles.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -45,6 +46,7 @@ class HomeScreen extends StatelessWidget {
     return IconButton(
       onPressed: onPressed,
       icon: Icon(icon),
+      color: AppTheme.medium,
     );
   }
 }
