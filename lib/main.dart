3d80904f26e08d054e5e0aa6c7b0e78348347965
Manuel Esteban Morales Zuarez <manuesteban1990@gmail.com>
2026-05-18 09:47:32 -0400

    feat: set home screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:metatube/screens/home_screen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomeScreen(),
+    );
   }
 }
